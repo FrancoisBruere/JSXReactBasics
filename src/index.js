d91f691d1cb1d764f436b065effe1277997e2ab3
Francois Bruere <francois.bruere@gmail.com>
2021-07-05 09:50:37 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Great Software</h1>
+    <ul>
+      <li>Javascript</li>
+      <li>React</li>
+      <li>Another item</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
